6d1705e33b9a56678dc09002333f1c170a26fec1
APCS2nd <APCS2nd@MHS-LT9-01>
2019-09-04 12:16:16 -0700

    calc

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,14 @@
+/* Jamin Chan
+ * 8/29/19 4th period
+ * 
+ * Group of methods that perform calcultions
+ */
+
+public class Calculate {
+	
+	public static void square(int number) {
+		return number * number;
+	}
+	
+	
+}
---
+++ Hello World Peoject/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*Jamin Chan 8/19/19 -say hello world*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hi Jamin!");
+
+	}
+
+}
