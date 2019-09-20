ec8a2742b6ac1951b8ab74d270a5419e996b8d49
Jamin-Chan <54374432+Jamin-Chan@users.noreply.github.com>
2019-09-20 00:53:26 -0700

    Update Calculate.java

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -6,24 +6,43 @@
 
 public class Calculate {
 	
+	/*A call to square returns the square of the value passed. 
+	The method accepts an integer and returns an integer. */
 	public static int square(int number) {
 		return number * number;
 	}
 	
+	/* A call to cube returns the cube of the value passed.
+	The method accepts an integer and returns an integer. */
 	public static int cube(int number ) {
 		return number * number * number;
 	}
 	
+	/*A call to average returns the average of the values passed to it.
+	This method accepts two doubles and returns a double. */
 	public static double average(double number1, double number2) {
 		return (number1 + number2) / 2;
 	}
 	
+	/*Another average method. This one accepts three doubles and returns a double.
+	The name is the same, the difference is in the header and the calculation.
+	(The ability to define two to more different methods with the same name but different method signitures
+	is called overloading. This avergae method is an example of "overloading method".) */
+	public static double average(double number1, double number2, double number3) {
+		
+	}
+	
+	/*A call to  toDegrees converts an angle measure given in radians into degrees. 
+	The method accepts a double and returns a double. 
+	The method should use 3.14159 as an approximatation for pi. */
 	public static double toDegrees(double radians) {
 		double degrees = 0;
 		degrees = (180 / 3.14159) * (radians);
 		return degrees;
 	}
 	
+	/* A call to toRadians converts an angle measure given in dgrees into radians.
+	The method accepts a double and returns a double. The method should use 3.14159 as an approximatation for pi. */
 	public static double toRadians(double degrees) {
 		double radians = 0;
 		radians = (3.14159 / 180) * (degrees);
@@ -36,12 +55,20 @@ public class Calculate {
 		return discriminant;
 	}
 	
+	
+	/*A call to toImproperFrac converts mixed number (with its pieces
+	provided separately in the order whole number, numerator, then denominator)
+	into an improper fraction. The method accepts a three integers and returns
+	a String. */
 	public static String toImproperFrac(int num1, int num2, int num3) {
 		int numerator = 0;
 		numerator = (num1 * num3) + num2;
 		return numerator + "/" + num3;
 	}
 	
+	/* A call to to toMixedNum converts an improper fraction (with its pieces provided
+	separately in the order numerator then denominator) into a mixed number.
+	The method accepts a two integers and returns a String. */
 	public static String toMixedNum(int num1, int num2) {
 		int wholeNum = num1 / num2;
 		int numerator = num1 % num2;
