7d50803ba6f8a86c0b9c3fe3f2e673263b19b555
Jamin-Chan <54374432+Jamin-Chan@users.noreply.github.com>
2019-10-25 11:57:25 -0700

    10/25/19

--- Hour Glass/src/hourGlass.java
+++ Hour Glass/src/hourGlass.java
@@ -2,20 +2,34 @@ import java.util.Scanner;
 
 public class hourGlass {
 	public static void main(String[] args) {
+		System.out.println("How Big?");
 		Scanner userInput = new Scanner(System.in);
 		int size = userInput.nextInt();
+		System.out.println();
 		
 		System.out.print("|");
-		for(int quote = 1, quote <= size, quote++;) {
+		for(int quote = 1; quote <= size; quote++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
 		
-		for(int line = 1, line <= size, line++) {
-			for(int space = 0, space < line, space++) {
+		for(int line = 0; line < size/2 - 1; line++) {
+			int space = 0;
+			for(space = 0; space < line + 1; space++) {
 				System.out.print(" ");
 			}
+
+			System.out.print("\\");
+			
+			for(int semi = 0; semi + space + space < size; semi++) {
+				System.out.print(":");
+			}
+			System.out.println("/");
 		}
+		
+		System.out.println("||");
+		
+		
 		
 		userInput.close();
 	}
