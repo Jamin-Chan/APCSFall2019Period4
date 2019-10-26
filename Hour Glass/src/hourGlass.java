2c1d94e615455aa3ea5623181d818501176d7264
Jamin-Chan <54374432+Jamin-Chan@users.noreply.github.com>
2019-10-25 21:22:35 -0700

    10/25/19

--- Hour Glass/src/hourGlass.java
+++ Hour Glass/src/hourGlass.java
@@ -13,23 +13,54 @@ public class hourGlass {
 		}
 		System.out.println("|");
 		
+		//top
 		for(int line = 0; line < size/2 - 1; line++) {
 			int space = 0;
+			
+			//for loop does the spaces
 			for(space = 0; space < line + 1; space++) {
 				System.out.print(" ");
 			}
 
 			System.out.print("\\");
 			
-			for(int semi = 0; semi + space + space < size; semi++) {
+			// loop does the colons
+			for(int colon = 0; colon + (space*2) < size; colon++) {
 				System.out.print(":");
 			}
+			
 			System.out.println("/");
 		}
 		
+		// middle
+		for(int i = 0; i < size/2; i++) {
+			System.out.print(" ");
+		}
 		System.out.println("||");
 		
+		//bottom
+		for(int line = 0; line < size/2 - 1; line++) {
+			int space = 0;
+			
+			for(space = 0; space < (size/2) - (line + 1); space++) {
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			
+			//bottom colon
+			for(int colon = size ; colon > (space *2) ; colon--) {
+				System.out.print(":");
+				
+			}
+			
+			System.out.println("\\");
+		}
 		
+		System.out.print("|");
+		for(int quote = 1; quote <= size; quote++) {
+			System.out.print("\"");
+		}
+		System.out.println("|");
 		
 		userInput.close();
 	}
