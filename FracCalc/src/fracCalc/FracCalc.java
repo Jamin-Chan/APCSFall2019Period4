c8cd7379f80dda96824a01e14e7e0c4d0965e4eb
APCS2nd <APCS2nd@MHS-LT9-01>
2019-11-06 12:46:54 -0800

    11/6/19

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -6,11 +6,16 @@ public class FracCalc {
 
     public static void main(String[] args) {
     	Scanner userInput = new Scanner(System.in);
+    	System.out.println("What is your equation?");
     	String equation = userInput.nextLine();
-    	//System.out.println(equation);
-    	produceAnswer(equation);
-
-        // TODO: Read the input from the user and call produceAnswer with an equation
+    	String stop = " ";
+  
+    	while(stop != "quit") {
+    		// TODO: Read the input from the user and call produceAnswer with an equation
+    		produceAnswer(equation);
+    		System.out.println("If stop, type 'quit'");
+    		stop = userInput.nextLine();
+    	}
     	userInput.close();
     }
     
@@ -25,7 +30,30 @@ public class FracCalc {
     public static String produceAnswer(String input) {
     	// TODO: Implement this function to produce the solution to the input
     	String numOpNum[] = input.split(" ");
-        return numOpNum[2];
+    	String num1[];
+    	String num2[];
+    	String split1Frac;
+    	String split2Frac;
+    	if(numOpNum[0].contains("_")) {
+    		num1 = numOpNum[0].split("_");
+    		num1 = num1[2].split("/");
+    		split1Frac = "whole:" + num1[0] + "numerator:" + num1[1] + "denominator:" + num1[2];
+    	} else {
+    		num1 = numOpNum[0].split("/");
+    		split1Frac =  "numerator:" + num1[0] + "denominator:" + num1[1];
+    	}
+    	
+    	if(numOpNum[2].contains("_")) {
+    		num2 = numOpNum[2].split("_");
+    		num2 = num2[1].split("/");
+    		split2Frac = "whole:" + num2[0] + "numerator:" + num2[1] + "denominator:" + num2[2];
+    	} else {
+    		num2 = numOpNum[2].split("/");
+    		split2Frac =  "numerator:" + num2[0] + "denominator:" + num2[1];
+    	}
+
+    	return split2Frac;
+
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
