dd4f6d5757c55e48e5b35aa039f9f2bd5b9f7a28
Jamin-Chan <54374432+Jamin-Chan@users.noreply.github.com>
2019-11-11 23:11:36 -0800

    11/11/19

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -4,7 +4,8 @@ public class Split {
 
  
 	public static void main(String[] args) {
-
+		System.out.println(part1("applespineapplesbreadlettucebreadtomatobaconmayohambreadcheese"));
+		System.out.println(part2("apples pineapples bread lettuce tomato bacon mayo ham bread cheese"));
 // Your task Part 0
 //String.split();
 //It's a method that acts on a string, <StringName>.split(<sp>);
@@ -24,7 +25,8 @@ public class Split {
 		System.out.println(Arrays.toString(outputArray3));
 		String[] outputArray4 = "really I really like really red apples!".split("really");
 		System.out.println(Arrays.toString(outputArray4));
-		
+		String[] outputArray5 = "I reallyreally likeapples".split("really");
+		System.out.println(Arrays.toString(outputArray5));
 		
 		
 //Your task Part 1:
@@ -39,9 +41,21 @@ public class Split {
 	}
 	
 	public static String part1(String sandwich) {
-		String[] ingredients = sandwich.split("bread");
-		sandwich.split("bread");
-		System.out.print(sandwich);
+		String inside = "";
+		System.out.println(Arrays.toString(sandwich.split("bread")));
+		String[] sandwichArray = sandwich.split("bread");
+		if(sandwichArray.length >= 2) {
+			for(int i = 1; i < sandwichArray.length - 1; i++) {
+				inside += sandwichArray[i];
+				if(i != sandwichArray.length - 2) {
+					inside += "bread";
+				}
+			}
+		} else {
+			return "Not a sandwich";
+		}
+
+		return inside;
 	}
 		
 //Your task pt 2:
@@ -56,11 +70,30 @@ public class Split {
 
 * the sandwich and ignores what's on the outside.
 
-* Again, what if it's a fancy sandwich with multiple pieces of bread?
+* Again, what if it's a fancy sandwich with multiple pieces of bread
 
 */
-
- }
-
+	public static String part2(String sandwich) {
+		String inside = "";
+		int index1 = 0;
+		int index2 = 0;
+		String[] sandwichArray = sandwich.split(" ");
+		for(int i = 0; i <= sandwichArray.length; i++) {
+			if(sandwichArray[i].equals("bread")) {
+				index1 = i + 1;
+				i = sandwichArray.length + 1;
+			}
+		}
+		for(int i = sandwichArray.length - 1; i >= 0; i--) {
+			if(sandwichArray[i].equals("bread")) {
+				index2 = i - 1;
+				i = -1;
+			}
+		}
+		
+		for(int i = index1; i <= index2; i++) {
+			inside += sandwichArray[i];
+		}
+		return inside;
+	}
 }
-
