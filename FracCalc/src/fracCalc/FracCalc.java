deb8b638a692cf56bc0efb49aafc773589b8ed2b
APCS2nd <APCS2nd@MHS-LT9-01>
2019-11-13 13:03:41 -0800

    11/13/19

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -32,43 +32,74 @@ public class FracCalc {
     public static String produceAnswer(String input) {
     	// TODO: Implement this function to produce the solution to the input
     	String numOpNum[] = input.split(" ");
-    	String whole1[];
-    	String whole2[];
-    	String frac1[];
-    	String frac2[];
-    	String split1Frac;
-    	String split2Frac;
-    	if(numOpNum[0].contains("_")) {
-    		whole1 = numOpNum[0].split("_");
-    		if(numOpNum[0].contains("/")) {
-    			frac1 = whole1[1].split("/");
-    			split1Frac = "whole:" + whole1[0] + " numerator:" + frac1[0] + " denominator:" + frac1[1];
-    		} else {
-    			split1Frac = "whole:" + whole1[0] + " numerator:0 denominator:1";
-    		}
-    	} else if(numOpNum[0].contains("/")){
-    		frac1 = numOpNum[0].split("/");
-    		split1Frac =  "whole:0 numerator:" + frac1[0] + " denominator:" + frac1[1];
-    	} else {
-    		split1Frac = "whole:" + numOpNum[0] + " numerator:0 denominator:1";
-    	}
+    	String num1 = numOpNum[0];
+    	String num2 = numOpNum[3];
+    	String separated1[] = separate(num1);
+    	String separated2[] = separate(num2);
     	
-    	if(numOpNum[2].contains("_")) {
-    		whole2 = numOpNum[2].split("_");
-    		if(numOpNum[2].contains("/")) {
-    			frac2 = whole2[1].split("/");
-    			split2Frac = "whole:" + whole2[0] + " numerator:" + frac2[0] + " denominator:" + frac2[1];
-    		} else {
-    			split2Frac = "whole:" + whole2[0] + " numerator:0 denominator:1";
-    		}
-    	} else if(numOpNum[2].contains("/")){							
-    		frac2 = numOpNum[2].split("/");
-    		split2Frac =  "whole:0 numerator:" + frac2[0] + " denominator:" + frac2[1];
-    	} else {
-    		split2Frac = "whole:" + numOpNum[2] + " numerator:0 denominator:1";
+    	
+    	//int whole = Integer.parseInt();
+    	if(numOpNum[1].equals("+")) {
+    		add(separated1, separated2);
+    	} else if(numOpNum[1].equals("-")) {
+    		
+    	} else if(numOpNum[1].equals("*")) {
+    		
+    	} else if(numOpNum[1].equals("/")) {
+    	
     	}
-
-    	return split2Frac;
+    	return "";
     }
+    
+    public static String[] separate(String num1) {
+    	
+    	String number[] = new String[3];
+    	number[0] = "0";
+    	number[1] = "0";
+    	number[2] = "1";
+    	
+    	if(num1.contains("/")) {
+    		String split[] = num1.split("/");
+    		number[2] = split[1];
+    	}
+    	if(num1.contains("_")) {
+    		String num[] = num1.split("_");
+    		number[0] = num[0];
+    		if(num[1].contains("/")) {
+    			String frac[] = num[1].split("/");
+    			number[1] = frac[0];
+    		}
+    	}	
+    	System.out.println(number[0]);
+    	return number;
+    }
+    
+    
+    public static void add(String[] num1, String[] num2) {
+    	
+   	}
+    
+    public static void substract(String[] num1, String[] num2) {
+    	
+   	}
+    	
+    public static void multiply(String[] num1, String[] num2) {
+	
+	}
+	
+    public static void divide(String[] num1, String[] num2) {
+    	
+	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     // TODO: Fill in the space below with any helper methods that you think you will need
 }
