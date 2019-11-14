aa6ef274a4cf8abe030495c860b825fcc0f0d9f4
APCS2nd <APCS2nd@MHS-LT9-01>
2019-11-13 16:40:50 -0800

    11/13/19

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,15 +1,10 @@
 package fracCalc;
-
 import java.util.Scanner;
-
 public class FracCalc {
-
     public static void main(String[] args) {
     	Scanner userInput = new Scanner(System.in);
     	boolean stop = false;
-  
     	while(stop == false) {
-    		// TODO: Read the input from the user and call produceAnswer with an equation
     		System.out.println("What is your equation?");
         	String equation = userInput.nextLine();
     		System.out.println(produceAnswer(equation));
@@ -20,44 +15,62 @@ public class FracCalc {
     	}
     	userInput.close();
     }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
-    	// TODO: Implement this function to produce the solution to the input
     	String numOpNum[] = input.split(" ");
     	String num1 = numOpNum[0];
-    	String num2 = numOpNum[3];
+    	String num2 = numOpNum[2];
     	String separated1[] = separate(num1);
     	String separated2[] = separate(num2);
-    	
-    	
-    	//int whole = Integer.parseInt();
+    	int whole1 = Integer.parseInt(separated1[0]);
+    	int numer1 = Integer.parseInt(separated1[1]);
+    	int den1 = Integer.parseInt(separated1[2]);
+    	int whole2 = Integer.parseInt(separated2[0]);
+    	int numer2 = Integer.parseInt(separated2[1]);
+    	int den2 = Integer.parseInt(separated2[2]);
+    	numer1 += whole1 * den1;
+    	numer2 += whole2 * den2;
+    	den1 *= den2;
+    	numer1 *= den1;
+    	numer2 *= den1;
     	if(numOpNum[1].equals("+")) {
-    		add(separated1, separated2);
-    	} else if(numOpNum[1].equals("-")) {
-    		
-    	} else if(numOpNum[1].equals("*")) {
-    		
+    		numer1 += numer2;
+    	}else if(numOpNum[1].equals("-")) {
+    		numer1 -= numer2; 
     	} else if(numOpNum[1].equals("/")) {
-    	
+    		numer1 *= den1;
+    		den1 *= numer2;
+    		if(den1 < 0) {
+    			numer1 *= -1;
+    			den1 *= -1;
+    		}
+    	}else if(numOpNum[1].equals("*")) {
+    		numer1 *= numer2;
+    		den1 *= den1;
     	}
-    	return "";
+    	int divisible = gcf(numer1, den1);
+    	if(divisible > 1) {
+    		numer1 /= divisible;
+    		den1 /= divisible;
+    	}
+    	if(numer1 > den1) {
+    		whole1 = numer1 / den1;
+    		numer1 -= (whole1 * numer1);
+    	}
+    	if(whole1 == 0 && numer1 != 0) {
+    		return numer1 + "/" + den1;
+    	} else if(numer1 == 0 && whole1 != 0 ) {
+    		return whole1 + "";
+    	} else if(numer1 != 0 && whole1 != 0) {
+    		return whole1 + "_" + numer1 + "/" + den1;
+    	} else {
+    		return "0";
+    	}
     }
-    
     public static String[] separate(String num1) {
-    	
     	String number[] = new String[3];
     	number[0] = "0";
     	number[1] = "0";
     	number[2] = "1";
-    	
     	if(num1.contains("/")) {
     		String split[] = num1.split("/");
     		number[2] = split[1];
@@ -70,36 +83,20 @@ public class FracCalc {
     			number[1] = frac[0];
     		}
     	}	
-    	System.out.println(number[0]);
     	return number;
     }
-    
-    
-    public static void add(String[] num1, String[] num2) {
-    	
-   	}
-    
-    public static void substract(String[] num1, String[] num2) {
-    	
-   	}
-    	
-    public static void multiply(String[] num1, String[] num2) {
-	
-	}
-	
-    public static void divide(String[] num1, String[] num2) {
-    	
-	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    // TODO: Fill in the space below with any helper methods that you think you will need
+    public static int gcf(int num1, int num2) {	
+    	int divisible = num2;
+    		while(divisible != 1) {
+				if(num1 % divisible == 0 && num2 % divisible == 0) {
+					return divisible;
+				}
+				if(num1 > 0) {
+					divisible--;
+				} else {
+					divisible++;
+				}
+			}
+    	return 1;
+    }   
 }
